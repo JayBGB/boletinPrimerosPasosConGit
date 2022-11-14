a540cf685e095ad6fd946b8d13166ab3fd5adcdd
Borja Garcia Barrera <borjagbarrera@gmail.com>
2022-11-14 17:04:32 +0100

    He añadido comentarios al archivo HolaMundo.java

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,5 +1,11 @@
 public class HolaMundo {
 
+/**
+*Muestra por pantalla la frase "¡Hola mundo!"
+*
+*@author Borja G. Barrera
+*/
+
 	public static void main (String[] args){
 	
 		System.out.print("¡Hola mundo!");
